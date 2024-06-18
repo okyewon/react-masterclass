18411020667a2b8cea859127abb4e5b85245bd5b
okwonye@gmail.com <okwonye@gmail.com>
2024-06-18 17:09:07 +0900

    프로젝트 생성 && Home 마크업 및 Coin 타입 지정

---
+++ src/routes/Coins.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0 10px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 15px;
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  letter-spacing: -0.02em;
+  a {
+    display: block;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+export default Coins;
