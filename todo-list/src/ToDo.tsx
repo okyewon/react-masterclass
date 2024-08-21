63262b7586af8099ab3ab6ac6ac996e8ceb281fe
okwonye@gmail.com <okwonye@gmail.com>
2024-08-21 17:12:58 +0900

    feat: todo-list가 담긴 atom 상태 변경 / 특정 todo의 category 변경하기

--- todo-list/src/ToDo.tsx
+++ todo-list/src/ToDo.tsx
@@ -1,9 +1,40 @@
-import { IToDo } from "./atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
-      {text} <button>Todo</button> <button>Doing</button> <button>Done</button>
+      {text}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          Todo
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
