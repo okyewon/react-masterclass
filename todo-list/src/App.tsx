8b95562e24cbf09fd5d4b8652994104e6158f213
okwonye@gmail.com <okwonye@gmail.com>
2024-08-23 18:32:02 +0900

    feat: selector 생성 및 get/set 메서드 사용해보기

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,11 +1,34 @@
+import { useRecoilState, useRecoilValue } from "recoil";
 import { createGlobalStyle } from "styled-components";
-import TodoList from "./TodoList";
+import { hourSelector, minutesState } from "./atoms";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
   return (
     <>
       <GlobalStyle />
-      <TodoList />
+      <div>
+        <input
+          value={minutes}
+          onChange={onMinutesChange}
+          type="number"
+          placeholder="Minutes"
+        />
+        <input
+          value={hours}
+          onChange={onHoursChange}
+          type="number"
+          placeholder="Hours"
+        />
+      </div>
     </>
   );
 }
